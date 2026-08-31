5246cf200faebd628af2478719461bd08a9068da
zmalkani <zmalkani@rsgc.on.ca>
2026-08-30 20:57:21 -0400

    Initial commit

---
+++ src/master/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <cmd.h>
+
+char nodeID = 'M';
+String ascii = "     __  ___________  __                    \n"
+                "   /  |/  / ____/ / / /                    \n"
+                "  / /|_/ / /   / / / /                     \n"
+                " / /  / / /___/ /_/ /                      \n"
+                "/_/__/_/\\____/\\____/______________________ \n"
+                "  / ____/ /   / / / / ___/_  __/ ____/ __ \\ \n"
+                " / /   / /   / / / /\\__ \\ / / / __/ / /_/ / \n"
+                "/ /___/ /___/ /_/ /___/ // / / /___/ _, _/  \n"
+                "\\____/_____/\____//____//_/ /_____/_/ |_|  ";
+                                                          
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(ascii);
+  Serial.println("Node: " + String(nodeID));
+  Serial.println("ESP32-S3");
+  Serial.println("-----------------------------");
+  Serial.println("Core: " + String(ESP.getCoreVersion()));
+  Serial.println("Chip: " + String(ESP.getChipModel()));
+  Serial.println("Cores: " + String(ESP.getChipCores()));
+  Serial.println("Revision: " + String(ESP.getChipRevision()));
+  Serial.println("CPU Freq: " + String(ESP.getCpuFreqMHz()) + " MHz");
+  Serial.println("Flash Size: " + String(ESP.getFlashChipSize() / (1024 * 1024)) + " MB");
+  Serial.println("Running: ");  
+  
+}
+
+void loop() {
+  String input = (String)Serial.read();
+  if (input.equals("ping")){
+    Serial.println("Ping");
+  }
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
---
+++ src/node/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
